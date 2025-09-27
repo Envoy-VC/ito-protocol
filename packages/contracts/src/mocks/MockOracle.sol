e1142d0b525e30995a99b4ce86e36fa4368cdccb
Vedant Chainani <vedantchainani1084@gmail.com>
2025-09-27 21:27:13 +0530

    chore: update mock oracle for token based pricing

--- packages/contracts/src/mocks/MockOracle.sol
+++ packages/contracts/src/mocks/MockOracle.sol
@@ -5,27 +5,27 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IOracle} from "../interfaces/IOracle.sol";
 
 contract MockOracle is Ownable {
-    uint256 public price;
-    uint256 public volatility;
+    // Token A => Token B => Price
+    mapping(address => mapping(address => uint256)) public price;
 
-    constructor(address initialOwner, uint256 _price, uint256 _volatility) Ownable(initialOwner) {
-        price = _price;
-        volatility = _volatility;
-    }
+    // Token A => Token B => Volatility
+    mapping(address => mapping(address => uint256)) public volatility;
+
+    constructor(address initialOwner) Ownable(initialOwner) {}
 
     function getPrice(address tokenA, address tokenB) public view returns (uint256) {
-        return price;
+        return price[tokenA][tokenB];
     }
 
     function getVolatility(address tokenA, address tokenB) public view returns (uint256) {
-        return volatility;
+        return volatility[tokenA][tokenB];
     }
 
     function setPrice(address tokenA, address tokenB, uint256 _newPrice) public onlyOwner {
-        price = _newPrice;
+        price[tokenA][tokenB] = _newPrice;
     }
 
     function setVolatility(address tokenA, address tokenB, uint256 _newVolatility) public onlyOwner {
-        volatility = _newVolatility;
+        volatility[tokenA][tokenB] = _newVolatility;
     }
 }
